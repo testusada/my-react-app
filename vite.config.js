895134b8eb60ae5924027ffe773f460fb0e8d7e0
HirokiTeshima <h.teshima18@gmail.com>
2025-03-30 09:04:40 +0900

    Clear cache and re-install dependencies

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
     host: "0.0.0.0", // ← ここを追加
     port: process.env.PORT || 5173, // Renderの環境変数PORTを使用
   },
+  preview: {
+    allowedHosts: ["my-react-app-1t3e.onrender.com"], // ← ここを追加
+  },
 });
